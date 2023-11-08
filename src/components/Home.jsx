d18aa47a6cb0f80fbed1cea29e9a290e5ab3f6f5
cywhale <cyrain@gmail.com>
2023-11-08 20:15:31 +0800

    Removed experimental components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,53 +1,18 @@
-import { useState, useEffect } from 'preact/hooks'
-import { Router, route } from 'preact-router'
+import { useState } from 'preact/hooks'
 import Swagger from './Swagger'
-import AsyncRoute from 'preact-async-route'
 import './Home.css'
 
-const base = '__ROUTE__'
-const baseurl = `/${base}`
-
 function Home() {
-  const [url, setUrl] = useState(`${baseurl}`)
-
-  useEffect(() => {route(url)}, [url])
-
-  const render_subRouter = () => {
-    const handlePageRoute = async e => {
-      console.log("Route to url: ", e.url)
-      if (e.url.indexOf('test') >= 0) {
-        //switch (e.url) {
-        //  case `${baseurl}/test`:
-        route(`${baseurl}/test`, true)
-        //break;
-      } else { //default:
-        route(`${baseurl}/`, true)
-      }
-    }
-
-    return(
-        <Router onChange={handlePageRoute}>
-          <AsyncRoute
-            path={`${baseurl}/test/:rest*`}
-            getComponent={() => import('./TestXarrow/index.jsx').then(module => module.default)}
-            loading={()=>{return <Loading url={`${baseurl}/test`}/>}}
-          />
-        </Router>
-    )
-  }
+  const [count, setCount] = useState(0)
 
   return (
     <div>
       <div>
-        <button type="button" onClick={() => setUrl(url => url.indexOf('test') >= 0? `${baseurl}/`: `${baseurl}/test`)}>URL is: {url}
+        <button type="button" onClick={() => setCount(count => count + 1)}>count is: {count}
         </button>
         <hr/><br/>
       </div>
-      <div id= "uicontainer">
-        <Swagger />
-        <div id="testdiv" />
-      </div>
-      { render_subRouter() }
+      <Swagger />
     </div>
   )
 }
--- src/components/Swagger/index.jsx
+++ src/components/Swagger/index.jsx
@@ -1,6 +1,4 @@
 import "swagger-ui-react/swagger-ui.css"
-//import SwaggerUI from 'swagger-ui-react'
-import DatePicker from "react-datepicker"
 import { Suspense, lazy, memo } from 'preact/compat'  //test dynamic loading
 
 const SwaggerUI = lazy(() => import('swagger-ui-react'))
@@ -12,66 +10,12 @@ const SwagX = (props) => (
     </div>
 )
 
-// https://gist.github.com/s-bauer/7ecfaf62c24a17c6a92c8599f414de37#file-app-js-L16
-const JsonSchema_string_date = (props) => {
-  const dateNumber = Date.parse(props.value);
-  const date = dateNumber
-    ? new Date(dateNumber)
-    : new Date();
-
-  return (
-    <DatePicker
-      selected={date}
-      onChange={d => props.onChange(d.toISOString().substring(0, 10))}
-    />
-  );
-}
-
-const JsonSchema_string_time = (props) => {
-  const dateNumber = Date.parse(props.value);
-  const date = dateNumber
-    ? new Date(dateNumber)
-    : new Date();
-
-  return (
-    <DatePicker
-      selected={date}
-      onChange={d => props.onChange(d.toISOString().substring(11))}
-    />
-  );
-}
-
-const JsonSchema_string_date_time = (props) => {
-  const dateNumber = Date.parse(props.value);
-  const date = dateNumber
-    ? new Date(dateNumber)
-    : new Date();
-
-  return (
-    <DatePicker
-      selected={date}
-      onChange={d => props.onChange(d.toISOString())}
-      showTimeSelect
-      timeFormat="p"
-      dateFormat="Pp"
-    />
-  )
-}
-
 const Swagger = (props) => {
-  const url = '/public/test04_03_noEnum.json'
+  const url = '/public/test04_02_noDatetime.json' //test04_02_noDatetime.json //test04_03_noEnum.json
               //'https://api.odb.ntu.edu.tw/search/schema?node=odb_v1_ctd_sadcp_bio_202210&num=1'
-  const DateTimeSwaggerPlugin = {
-    components: {
-      "JsonSchema_string_time": JsonSchema_string_time,
-      "JsonSchema_string_date": JsonSchema_string_date,
-      "JsonSchema_string_date-time": JsonSchema_string_date_time
-    }
-  }
-
   return(
     <Suspense fallback={<Loading />}>
-        <SwagX url={url} plugins={[DateTimeSwaggerPlugin]} />
+        <SwagX url={url} />
     </Suspense>
   )
 }
